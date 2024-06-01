b06f3ec392ad53c14bef466ddc7e334b9b67a54b
Indira Yarnagula <indhu.sony777@gmail.com>
2024-06-01 22:09:16 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, jsonify
 import random
 
-app = Flask(__name__)
+class QuoteApp(Flask):
+    def __init__(self, import_name):
+        super().__init__(import_name)
+        self.quotes = [
+            "The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+            "The purpose of our lives is to be happy. - Dalai Lama",
+            "Life is what happens when you’re busy making other plans. - John Lennon",
+            "Get busy living or get busy dying. - Stephen King",
+            "You have within you right now, everything you need to deal with whatever the world can throw at you. - Brian Tracy"
+        ]
+        self.route("/", methods=["GET"])(self.index)
+        self.route("/quote", methods=["GET"])(self.get_quote)
 
-# Predefined list of quotes
-quotes = [
-    "The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
-    "The purpose of our lives is to be happy. - Dalai Lama",
-    "Life is what happens when you’re busy making other plans. - John Lennon",
-    "Get busy living or get busy dying. - Stephen King",
-    "You have within you right now, everything you need to deal with whatever the world can throw at you. - Brian Tracy"
-]
+    def index(self):
+        return render_template("index.html")
 
-@app.route('/quote', methods=['GET'])
-def get_quote():
-    quote = random.choice(quotes)
-    return jsonify({"quote": quote})
+    def get_quote(self):
+        quote = random.choice(self.quotes)
+        return jsonify({"quote": quote})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+app = QuoteApp(__name__)
